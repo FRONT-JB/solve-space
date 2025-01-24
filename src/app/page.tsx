d45ec305f723924b466a66784e9d50665086e227
front-jb <jeonbg@kakao.como>
2025-01-25 01:27:56 +0900

    🔧 config: supabase 설정 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
+import { createClient } from "../../lib/supabase/server";
+
+export default async function Home() {
+  const supabase = await createClient();
+  const { data: countries } = await supabase.from("countries").select();
+
+  console.log(countries);
   return <div>Home</div>;
 }
