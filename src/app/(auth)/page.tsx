9424cfd23b3e84e606105939b2f83971d1e3b60e
전병길 <jeonbg@kakao.com>
2025-01-25 22:14:59 +0900

    💄 style: 테마토글 스타일 수정

--- src/app/(auth)/page.tsx
+++ src/app/(auth)/page.tsx
@@ -8,13 +8,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-10 h-full pt-10">
+    <div className="flex flex-col gap-5 md:gap-10 h-full py-4 md:py-10">
       <div className="flex flex-col items-center justify-center gap-4">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           className="inline-flex items-center gap-2 px-4 py-1.5 rounded-full bg-gradient-to-r
-             from-blue-500/30 to-purple-500/30 dark:from-blue-500/10 dark:to-purple-500/10 text-sm text-slate-800 dark:text-gray-400 cursor-default"
+             from-blue-500/35 to-purple-500/35 dark:from-blue-500/25 dark:to-purple-500/25 text-sm text-slate-800 dark:text-gray-400 cursor-default"
         >
           <BookOpen className="w-4 h-4" />
           문제를 해결하는 공간
@@ -24,7 +24,7 @@ export default function Home() {
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 0.1 }}
-          className="text-4xl md:text-5xl font-bold bg-gradient-to-r from-slate-600 to-slate-500 dark:from-gray-100 dark:to-gray-300 text-transparent bg-clip-text cursor-default"
+          className="text-4xl md:text-5xl text-center font-bold bg-gradient-to-r from-slate-600 to-slate-500 dark:from-gray-100 dark:to-gray-300 text-transparent bg-clip-text cursor-default"
         >
           Discover & Share Code Snippets
         </motion.h1>
@@ -51,11 +51,11 @@ export default function Home() {
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.4 }}
-        className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
+        className="grid gap-3 md:gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 pb-20 md:pb-4"
         layout
       >
         <AnimatePresence mode="popLayout">
-          {[1, 2, 3, 4, 5, 6, 7].map((snippet) => (
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((snippet) => (
             <motion.div
               key={snippet}
               layout
--- src/app/(auth)/ui/ThemeToggle.tsx
+++ src/app/(auth)/ui/ThemeToggle.tsx
@@ -16,7 +16,7 @@ export default function ThemeToggle() {
   const { setTheme } = useTheme();
 
   return (
-    <div className="fixed bottom-4 right-4">
+    <div className="fixed bottom-6 right-6">
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="outline" size="icon">
