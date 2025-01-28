69bb006f863caccb28b809e29fc44d3c7d85b901
전병길 <jeonbg@kakao.com>
2025-01-28 17:09:08 +0900

    🐛 fix: snippet 상세 페이지 params 타입수정

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -5,7 +5,7 @@ import { Snippet } from "../edit/_types";
 export default async function SnippetDetail({
   params,
 }: {
-  params: Readonly<{ id: string }>;
+  params: Promise<{ id: string }>;
 }) {
   // 이미 resolved된 값이지만 await을 사용하지 않으면 Nextjs 타입시스템에서 Promise일 것으로 예상해 에러가 발생함
   const { id: snippetId } = await params;
